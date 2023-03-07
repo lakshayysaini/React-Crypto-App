c72e504c37f85e58e94d281b39d74557b2214a1c
lakshayysaini <lakshayysaini@gmail.com>
2023-03-08 00:01:04 +0530

    going a bit slow but some header shit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import { Layout, Space, Typography } from "antd";
 
-import {Navbar} from "./components";
-import './App.css';
+import { Navbar } from "./components";
+import "./App.css";
 
 const App = () => {
-
-    return (
-        <div className="app">
-            <div className="navbar"> 
-                <Navbar/>
-            </div>
-            <div className="main">
-            </div>
-            <div className="footer">
-            </div>
-        </div>
-    );
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes"></div>
+        </Layout>
+      </div>
+      <div className="footer"></div>
+    </div>
+  );
 };
 
 export default App;
