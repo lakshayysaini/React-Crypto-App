e7eaec762b33914e082ac46b829c7d530424ea50
lakshayysaini <lakshayysaini@gmail.com>
2023-03-10 23:02:32 +0530

    API connected with store Store passed in provider App wrapped in Provider Footer created

--- src/App.js
+++ src/App.js
@@ -34,8 +34,22 @@ const App = () => {
             </Routes>
           </div>
         </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Kryptoverse
+            <br />
+            All Rights Reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
-      <div className="footer"></div>
     </div>
   );
 };
